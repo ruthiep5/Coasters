a9e72221195390174539676a9dd071d0d7cef6c8
ruthiep5 <82891285+ruthiep5@users.noreply.github.com>
2023-02-08 14:37:01 -0600

    .

--- test.php
+++ test.php
@@ -7,8 +7,8 @@ echo "and you too coen!<br>";
 print_r($_GET);
 echo "Heloo " . $_GET["name"];
 
-foreach ($_GET as $id){
-    echo $id . "==>" . $_GET[$id] . "<br>";
+foreach ($_GET as $id => $val){
+    echo $id . "==>" . $val . "<br>";
 }
 
 $f=fopen("test.json", "a");
