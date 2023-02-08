45d0662a26c6011c7c0629718cdf0c6f652ea107
ruthiep5 <82891285+ruthiep5@users.noreply.github.com>
2023-02-08 14:34:58 -0600

    .

--- test.php
+++ test.php
@@ -1,11 +1,15 @@
 <?php
 
-echo "Hi <b>Brooke</b>, why are you late?";
-echo "and you too coen!";
+echo "Hi <b>Brooke</b>, why are you late? ";
+echo "and you too coen!<br>";
 
 //echo $_GET;
 print_r($_GET);
 echo "Heloo " . $_GET["name"];
+
+foreach ($_GET as $id){
+    echo $id . "==>" . $_GET[$id] . "<br>";
+}
 
 $f=fopen("test.json", "a");
 fwrite($f, "this is a file");
