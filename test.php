0d612d51ab80177ebc190d1611ab56f38ce36960
ruthiep5 <82891285+ruthiep5@users.noreply.github.com>
2023-02-09 13:16:08 -0600

    .

--- test.php
+++ test.php
@@ -8,8 +8,8 @@ print_r($_GET);
 echo "Hello" . $_GET["name"];
 echo <br>
 foreach ($_GET as $id => $val){
-    echo $id . "==>" . $val . "<br>";
-}
+    echo $id . "==>" . $val . "<br>"
+};
 
 $f=fopen("test.json", "a");
 fwrite($f, "this is a file");
