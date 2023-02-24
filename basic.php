747a89ba9d25f81d0fa9bc359cefd12baff8c3ea
ruthiep5 <82891285+ruthiep5@users.noreply.github.com>
2023-02-24 14:27:32 -0600

    .

--- basic.php
+++ basic.php
@@ -4,4 +4,22 @@
    // echo $_GET["name"];
     print_r($_GET);
 
+    // get information from the url parameters if one of the parameters in called 'name'
+    if (!empty($_GET['firstName'])){
+        
+        $_GET["time"] = time();
+        $_GET['saved'] = True;
+
+        //save info to database file
+        $dbFileName = 'coaster.json';
+
+        $file = fopen($dbFileName, 'r');
+        fwrite($file, json_encode($_GET)."\n");
+        fclose($file);
+        //send the information back as confimration
+        echo json_encode($_GET);
+    } else {
+        echo "DATA NOT SAVED";
+        
+    }
 ?>
